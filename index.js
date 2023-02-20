9f06f789374dbd775cdc2f937eb7fc85d65c9365
sdjhen <114769817+sdjhen@users.noreply.github.com>
2023-02-20 16:31:17 +0000

    added questions and tested using node

--- index.js
+++ index.js
@@ -2,22 +2,82 @@ const fs = require('fs');
 const path = require('path');
 const inquirer = require('inquirer');
 const generateMarkdown = require('./utils/generateMarkdown');
+const { type } = require('os');
 
 // array of questions for user
-const questions = [];
+const questions = [
+  // Title
+  {
+    type: 'input',
+    name: 'name',
+    message: 'Hello! Please choose the name of your project?',
+  },
+  // Description
+  {
+    type: 'input',
+    name: 'Description',
+    message: 'Now, please add a description of your project.',
+  },
+  // Installation guide
+  {
+    type: 'input',
+    name: 'Installation Guide',
+    message: 'Do you have any installation instructions for your project?',
+  },
+  // Usage
+  {
+    type: 'input',
+    name: 'Usage',
+    message: 'Can you provide a guide for how your project should be used?',
+  },
+  // License
+  {
+    type: 'list',
+    choices: ['MIT', 'GPL', 'BSD', 'APACHE'],
+    name: 'license',
+    message: 'What is your project license type?',
+  },
+  // Contributions
+  {
+    type: 'input',
+    name: 'Contributions',
+    message: 'Hello! Please choose the name of your project?',
+  },
+  // Tests
+  {
+    type: 'input',
+    name: 'Tests',
+    message: 'How should the project be tested?',
+  },
+  // Questions
+  {
+    type: 'input',
+    name: 'Questions',
+    message: 'Is there any additional questions?',
+  },
+  // Github Username
+  {
+    type: 'input',
+    name: 'Github',
+    message: 'What is your Github Username? @[username]',
+  },
+  // Email
+  {
+    type: 'input',
+    name: 'Email',
+    message: 'What is your Email?',
+  },
+];
 
 // function to write README file
 function writeToFile(fileName, data) {}
 
 // function to initialize program
 function init() {
-  inquirer
-    .prompt([
-      /*  questions  */
-    ])
-    .then((answers) => {
-      // User feedback
-    });
+  inquirer.prompt(questions).then((answers) => {
+    // User feedback
+    console.log(answers);
+  });
 }
 
 // function call to initialize program
