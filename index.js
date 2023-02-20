63b7a61c19321c1d39210e72bfa821025a107c0a
sdjhen <114769817+sdjhen@users.noreply.github.com>
2023-02-20 14:20:58 +0000

    started init function using inquirer js

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
-const fs = require("fs");
+const fs = require('fs');
 const path = require('path');
-const inquirer = require("inquirer");
-const generateMarkdown = require("./utils/generateMarkdown");
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // array of questions for user
-const questions = [
-
-];
+const questions = [];
 
 // function to write README file
-function writeToFile(fileName, data) {
-}
+function writeToFile(fileName, data) {}
 
 // function to initialize program
 function init() {
-
+  inquirer
+    .prompt([
+      /*  questions  */
+    ])
+    .then((answers) => {
+      // User feedback
+    });
 }
 
 // function call to initialize program
